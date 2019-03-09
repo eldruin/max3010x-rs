2a9edcd10a8b7c15729bc3446024ddfdc7baaff5
Diego Barrios Romero <eldruin@gmail.com>
2019-03-09 13:37:43 +0100

    Improve usage example

--- examples/linux.rs
+++ examples/linux.rs
@@ -3,7 +3,7 @@ extern crate linux_embedded_hal;
 extern crate max3010x;
 
 use linux_embedded_hal::I2cdev;
-use max3010x::Max3010x;
+use max3010x::{Led, Max3010x, SampleAveraging};
 
 fn main() {
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
@@ -12,4 +12,13 @@ fn main() {
 
     // This should print "Part ID 0x15" for a MAX30102.
     println!("Part ID: {}", part_id);
+
+    let mut sensor = sensor.into_heart_rate().unwrap();
+    sensor.set_sample_averaging(SampleAveraging::Sa4).unwrap();
+    sensor.set_pulse_amplitude(Led::All, 15).unwrap();
+    sensor.enable_fifo_rollover().unwrap();
+    let mut data = [0; 3];
+    let samples_read = sensor.read_fifo(&mut data).unwrap();
+
+    println!("Samples read: {}", samples_read);
 }
